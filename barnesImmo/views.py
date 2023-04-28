232f645b720bb0be7c0cd81a9a4d8cea34b69f36
Jean Balangue <jeanbalangue@hotmail.fr>
2023-04-28 19:39:02 +0200

    ajout request et 1er scraping

--- barnesImmo/views.py
+++ barnesImmo/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 def home(request):
-     return render(request, 'barnesImmo/home.html')
+    url = "https://locationsaintjeandeluz.fr"
+    headers = {
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+
+    context={}
+    context["titre"] = str(soup.h1)
+    print(context["titre"])
+
+    return render(request, 'barnesImmo/home.html', context)
